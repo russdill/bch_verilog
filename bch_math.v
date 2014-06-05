2787b7049ec7edcd44c089070c93df41e5aea675
Russ Dill <Russ.Dill@gmail.com>
2014-06-05 05:04:52 -0700

    Add a finite_serial_adder

--- bch_math.v
+++ bch_math.v
@@ -320,6 +320,27 @@ module finite_parallel_adder #(
 	end
 endmodule
 
+module finite_serial_adder #(
+	parameter M = 4
+) (
+	input clk,
+	input start,
+	input ce,
+	input [M-1:0] parallel_in,
+	input serial_in,
+	output reg [M-1:0] parallel_out = 0,
+	output serial_out
+);
+	localparam TCQ = 1;
+
+	always @(posedge clk)
+		if (start)
+			parallel_out <= #TCQ {parallel_in[0+:M-1], parallel_in[M-1] ^ serial_in};
+		else if (ce)
+			parallel_out <= #TCQ {parallel_out[0+:M-1], parallel_out[M-1] ^ serial_in};
+	assign serial_out = parallel_out[0];
+endmodule
+
 module lfsr_counter #(
 	parameter M = 4
 ) (
