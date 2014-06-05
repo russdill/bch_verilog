828b4cf3a53e51ff723d0d3dae88a8d66b7f1e16
Russ Dill <Russ.Dill@gmail.com>
2014-06-04 18:26:05 -0700

    Improve readability of speed of encoder polynomial generation

--- bch_encode.v
+++ bch_encode.v
@@ -14,61 +14,52 @@ module bch_encode #(
 
 `include "bch.vh"
 
+/* Calculate least common multiple which has x^2t .. x as its roots */
 function [(1<<MAX_M)-1:0] encoder_poly;
 	input [31:0] m;
 	input [31:0] t;
-	integer nk1;
 	integer nk;
-	integer s;
-	integer next_s;
 	integer i;
-	integer b;
-	integer k;
-	integer c;
-	integer done;
+	integer j;
+	integer n;
+	integer a;
 	integer curr;
 	integer prev;
 	integer ret;
-	reg [(1<<MAX_M)*1024-1:0] poly; /* FIXME: 1024 Not big enough for M=16 */
-
+	reg [(1<<MAX_M)*MAX_M-1:0] poly;
+	reg [(1<<MAX_M)-1:0] roots;
 begin
-	poly[0*(1<<MAX_M)+:1<<MAX_M] = 1;
-	for (i = 1; i < 1024; i = i + 1)
-		poly[i*(1<<MAX_M)+:1<<MAX_M] = 0;
+	n = m2n(m);
 
-	nk1 = m;
-	nk = 0;
-	s = 1;
-	b = 2;
-	while (2 * t - 1 >= s) begin
-
-		c = b;
-		done = 0;
-		while (!done) begin
-			prev = 0;
-			for (i = 0; i < nk1; i = i + 1) begin
-				curr = poly[i*(1<<MAX_M)+:1<<MAX_M];
-				poly[i*(1<<MAX_M)+:1<<MAX_M] = finite_mult(m, curr, c) ^ prev;
-				prev = curr;
-			end
-			poly[i*(1<<MAX_M)+:1<<MAX_M] = prev;
-			nk = nk + 1;
-			c = finite_mult(m, c, c);
-			if (c == b)
-				done = 1;
+	/* Calculate the roots for this finite field */
+	roots = 0;
+	for (i = 0; i < t; i = i + 1) begin
+		a = 2 * i + 1;
+		for (j = 0; j < m; j = j + 1) begin
+			roots[a] = 1;
+			a = (2 * a) % n;
 		end
-
-		next_s = next_syndrome(m, s);
-		for (i = 0; i < next_s - s; i = i + 1)
-			b = mul1(m, b);
-		s = next_s;
-		nk1 = nk + m;
 	end
 
-	k = m2n(m) - nk;
+	nk = 0;
+	poly[0*MAX_M+:MAX_M] = 1;
+	for (i = 0; i < n; i = i + 1) begin
+		if (roots[i]) begin
+			prev = 0;
+			a = lpow(m, i);
+			poly[(nk+1)*MAX_M+:MAX_M] = 1;
+			for (j = 0; j <= nk; j = j + 1) begin
+				curr = poly[j*MAX_M+:MAX_M];
+				poly[j*MAX_M+:MAX_M] = finite_mult(m, curr, a) ^ prev;
+				prev = curr;
+			end
+			nk = nk + 1;
+		end
+	end
+
 	ret = 0;
 	for (i = 0; i < nk; i = i + 1) begin
-		if (|poly[i*(1<<MAX_M)+:1<<MAX_M])
+		if (|poly[i*MAX_M+:MAX_M])
 			ret = ret | (1 << i);
 	end
 	encoder_poly = ret;
@@ -78,6 +69,7 @@ endfunction
 
 localparam TCQ = 1;
 localparam M = n2m(N);
+localparam ENC = encoder_poly(M, T);
 
 reg [N-K-1:0] lfsr = 0;
 wire [M-1:0] count;
@@ -105,7 +97,7 @@ always @(posedge clk) begin
 	if (reset)
 		lfsr <= #TCQ 0;
 	else
-		lfsr <= #TCQ {lfsr[N-K-2:0], 1'b0} ^ ({N-K{lfsr_in}} & encoder_poly(M, T));
+		lfsr <= #TCQ {lfsr[N-K-2:0], 1'b0} ^ ({N-K{lfsr_in}} & ENC);
 
 	data_out <= #TCQ vdin ? data_in : lfsr[N-K-1];
 end
