17f4816e3cf2575440d828cc6dca6df2361a4942
Russ Dill <Russ.Dill@gmail.com>
2014-06-06 19:29:29 -0700

    Reduce the number of multipliers in parallel decoding by adding a pipeline register

--- tmec_decode_parallel.v
+++ tmec_decode_parallel.v
@@ -22,12 +22,12 @@ module tmec_decode_parallel #(
 
 	localparam TCQ = 1;
 
-	reg [M-1:0] d_r = 0;
+	reg [M-1:0] d_r = 0;	/* In !shuffle cycle d_r is d_p */
 	wire [M-1:0] d_r_next;
 	reg [M-1:0] d_p = 0;
 	wire [M*(T+1)-1:0] d_r_terms;
-	wire [M*(T+1)-1:0] d_p_sigma;
-	wire [M*(T+1)-1:0] d_r_beta;
+	wire [M*(T+1)-1:0] product;
+	reg [M*(T+1)-1:0] d_r_beta;
 	reg [M*(T+1)-1:0] beta = 0;
 	reg busy = 0;
 
@@ -44,6 +44,7 @@ module tmec_decode_parallel #(
 	assign sigma0 = {syn1, {M-1{1'b0}}, 1'b1};
 
 	assign d_r_nonzero = |d_r;
+	reg [M*(T+1)-1:0] in2;
 
 	always @(posedge clk) begin
 		if (syn_done) begin
@@ -59,39 +60,45 @@ module tmec_decode_parallel #(
 		ch_start <= #TCQ busy && syn_shuffle && bch_n == T-1;
 			
 		if (syn_done) begin
+			d_r <= #TCQ syn1 ? syn1 : 1;
 			d_p <= #TCQ syn1 ? syn1 : 1;
+			in2 <= #TCQ sigma0;
 			sigma <= #TCQ sigma0;
 			beta <= #TCQ beta0;
-		end else if (busy && !syn_shuffle)
+		end else if (busy && !syn_shuffle) begin
 			d_r <= #TCQ d_r_next;
-		else if (busy) begin
+			d_r_beta <= #TCQ product;
+			in2 <= #TCQ beta;
+		end else if (busy) begin
 			/* d_p = bsel ? d_r : d_p */
-			if (bsel)
+			if (bsel) begin
 				d_p <= #TCQ d_r;
+			end else
+				d_r <= #TCQ d_p;
 
 			/* sigma^(r)(x) = d_p * sigma^(r-1)(x) - d_r * beta^(r)(x) */
-			sigma <= #TCQ {d_p_sigma ^ d_r_beta};
+			sigma <= #TCQ d_r_beta ^ product;
+			in2 <= #TCQ d_r_beta ^ product;
 
 			/* b^(r+1)(x) = x^2 * (bsel ? sigmal^(r-1)(x) : b_(r)(x)) */
 			beta[2*M+:M*(T-1)] <= #TCQ bsel ? sigma[0*M+:M*(T-1)] : beta[0*M+:M*(T-1)];
 		end
 	end
 
-	/* d_r * beta^(r)(x) */
-	parallel_standard_multiplier #(M, T+1) u_mbn(
+	/* d_r * beta^(r)(x), d_p * sigma^(r-1)(x) */
+	parallel_standard_multiplier #(M, T+1) u_d_p_sigma(
 		.standard_in1(d_r),
-		.standard_in2(beta),
-		.standard_out(d_r_beta)
+		.standard_in2(in2),
+		.standard_out(product)
 	);
 
-	for (i = 0; i <= T; i = i + 1) begin : parallel_standard_multiplier
-		/* d_r_terms = {sigma_i^(r) * S_(2 * r - i + 1)}[0..t], d_p * sigma^(r-1)(x) */
-		parallel_standard_multiplier #(M, 2) u_mn(
-			.standard_in1(sigma[i*M+:M]),
-			.standard_in2({syn_shuffled[i*M+:M], d_p}),
-			.standard_out({d_r_terms[i*M+:M], d_p_sigma[i*M+:M]})
-		);
-	end
+	/* d_r_terms = {sigma_i^(r) * S_(2 * r - i + 1)}[0..t] */
+	/* Only used for syn_shuffle cycle */
+	parallel_standard_multiplier #(M) u_d_r_terms [T:0] (
+		.standard_in1(sigma),
+		.standard_in2(syn_shuffled[0+:(T+1)*M]),
+		.standard_out(d_r_terms)
+	);
 
 	/* d_r = summation of dr_terms */
 	finite_parallel_adder #(M, T+1) u_generate_cs(d_r_terms, d_r_next);
